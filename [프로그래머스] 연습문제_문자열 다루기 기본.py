ae76546431c3f5915127605a2b11c09d9e6387d9
Yoonyesol <maniac970430@naver.com>
2022-05-17 22:10:53 +0900

    [PGM] 문자열 다루기 기본 / level 1 / 7분

---
+++ [프로그래머스] 연습문제_문자열 다루기 기본.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = True
+    for i in s:
+        answer = False
+        if len(s)==4 or len(s)==6:
+            if s.isdigit():
+                answer = True
+    return answer
