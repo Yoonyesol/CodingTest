a1a2db5bfbc2607a315d30053aa6f8610af0bfea
Yoonyesol <maniac970430@naver.com>
2022-06-25 20:37:24 +0900

    [PGM] 최소직사각형 / level 1 / 50분

---
+++ [프로그래머스] 위클리 챌린지_최소직사각형.py
@@ -0,0 +1,13 @@
+def solution(sizes):
+    big = []
+    small = []
+    for i in sizes:
+        if i[0]>i[1]:
+            big.append(i[0])
+            small.append(i[1])
+        else:
+            big.append(i[1])
+            small.append(i[0])
+    big.sort()
+    small.sort()
+    return big[-1]*small[-1]
