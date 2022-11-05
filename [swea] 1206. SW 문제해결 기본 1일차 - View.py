a4550e1ef8ab886c86c1460b352c8aaf398df626
Yoonyesol <maniac970430@naver.com>
2022-11-06 00:14:15 +0900

    [SWEA] [S/W 문제해결 기본] 1일차 - view (ver2) / D3

--- [swea] 1206. SW 문제해결 기본 1일차 - View.py
+++ [swea] 1206. SW 문제해결 기본 1일차 - View.py
@@ -14,3 +14,20 @@ for test_case in range(1, 11):
     if middle - max(arr) > 0:
     	sum = sum + (middle - max(arr))
     print("#{} {}".format(test_case, sum))
+
+
+-----------------------------------------------
+
+
+for test_case in range(1, 11):
+    a = int(input())
+    n = list(map(int, input().split()))
+    sum = 0
+    for i in range(2, a-2):
+        view1 = n[i]-n[i-2]
+        view2 = n[i]-n[i-1]
+        view3 = n[i]-n[i+1]
+        view4 = n[i]-n[i+2]
+        if view1 > 0 and view2 > 0 and view3 > 0 and view4 > 0 :
+            sum = sum + min(view1, view2, view3, view4)
+    print("#{} {}".format(test_case, sum))
