2351e8a6403e1da6db84bdcfda716b6142247211
Yoonyesol <maniac970430@naver.com>
2022-12-26 23:14:42 +0900

    [PGM] 멀리 뛰기 / level 2 / 45분

---
+++ [프로그래머스] 연습문제_멀리 뛰기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    if n <= 3:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1): #피보나치 수 구하기
+        dp[i] = dp[i-2] + dp[i-1]
+    return dp[n] % 1234567
