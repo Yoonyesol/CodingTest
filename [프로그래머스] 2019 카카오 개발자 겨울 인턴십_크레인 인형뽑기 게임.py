3003be9ed51b2e69d047f75a4ba4c5612222f6f0
Yoonyesol <maniac970430@naver.com>
2023-01-04 14:56:13 +0900

    [PGM] 크레인 인형뽑기 게임(효율성 개선) / level 1 / ?분

--- [프로그래머스] 2019 카카오 개발자 겨울 인턴십_크레인 인형뽑기 게임.py
+++ [프로그래머스] 2019 카카오 개발자 겨울 인턴십_크레인 인형뽑기 게임.py
@@ -14,3 +14,21 @@ def solution(board, moves):
                         board[j][i] = 0
                         break                    
     return answer
+
+----------------------
+불필요한 for문 제거
+
+def solution(board, moves):
+    answer = 0
+    stack = []
+    for i in moves:
+        if len(stack) >= 2 and stack[-1] == stack[-2]:
+            stack.pop()
+            stack.pop()
+            answer += 2
+        for j in range(len(board[0])):
+            if board[j][i-1] != 0:
+                stack.append(board[j][i-1])
+                board[j][i-1] = 0
+                break                    
+    return answer
