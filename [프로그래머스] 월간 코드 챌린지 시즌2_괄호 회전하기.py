3f15e4ed8fcfc93f0271cfa47c73b094465cc932
Yoonyesol <maniac970430@naver.com>
2023-01-11 23:49:05 +0900

    [PGM] 괄호 회전하기(효율성 개선) / level 2 / ?분

--- [프로그래머스] 월간 코드 챌린지 시즌2_괄호 회전하기.py
+++ [프로그래머스] 월간 코드 챌린지 시즌2_괄호 회전하기.py
@@ -4,10 +4,6 @@ def solution(s):
     for i in range(len(s_list)-1):
         stack = []
         for j in s_list:
-            if len(stack) > 2:
-                if (stack[-1] == ']' and stack[-2] == '[') or (stack[-1] == '}'and stack[-2] == '{') or (stack[-1] == ')' and stack[-2] == '('):
-                    stack.pop()
-                    stack.pop()
             if stack:
                 if (j == ']' and stack[-1] == '[') or (j == '}'and stack[-1] == '{') or (j == ')' and stack[-1] == '('):
                     stack.pop()
