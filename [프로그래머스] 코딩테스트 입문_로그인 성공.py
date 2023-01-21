6a72de2206d5aed01ab7b60a5b77882575e20634
Yoonyesol <maniac970430@naver.com>
2023-01-21 23:42:31 +0900

    [PGM] 로그인 성공? / level 0 / 6분 34초

---
+++ [프로그래머스] 코딩테스트 입문_로그인 성공.py
@@ -0,0 +1,11 @@
+def solution(id_pw, db):
+    answer = ''
+    for i in db:    #로그인 성공
+        if id_pw[0] != i[0]:
+            answer = "fail"
+        if id_pw[0] == i[0]:
+            if id_pw[1] == i[1]:
+                return "login"
+            else:
+                return "wrong pw"
+    return answer
