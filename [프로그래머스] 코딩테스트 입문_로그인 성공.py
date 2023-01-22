ad93da645a58a1a117fbd50986501955a56cb620
Yoonyesol <maniac970430@naver.com>
2023-01-22 17:27:29 +0900

    [PGM] 로그인 성공?(코드 개선) / level 0 / ?분

--- [프로그래머스] 코딩테스트 입문_로그인 성공.py
+++ [프로그래머스] 코딩테스트 입문_로그인 성공.py
@@ -9,3 +9,13 @@ def solution(id_pw, db):
             else:
                 return "wrong pw"
     return answer
+
+--------------------------------------
+def solution(id_pw, db):
+    for i in db:
+        if id_pw[0] == i[0]:
+            if id_pw[1] == i[1]:
+                return "login"
+            else:
+                return "wrong pw"
+    return "fail"
