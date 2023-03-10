4e5e0ada4a9534279b121e161c6bbbd78f1c8a1a
Yoonyesol <maniac970430@naver.com>
2023-03-10 18:36:31 +0900

    [PGM] 영어가 싫어요 / level 0 / 5분(py-re)

--- [프로그래머스] 코딩테스트 입문_영어가 싫어요.py
+++ [프로그래머스] 코딩테스트 입문_영어가 싫어요.py
@@ -3,3 +3,13 @@ def solution(numbers):
     for i, j in zip(alpha, range(10)):
         numbers = numbers.replace(i, str(j))
     return int(numbers)
+
+---------------------------
+
+def solution(numbers):
+    answer = 0
+    num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for i in num:
+        if i in numbers:
+            numbers = numbers.replace(i, str(num.index(i)))
+    return int(numbers)
