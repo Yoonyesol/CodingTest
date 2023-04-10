59ac367e1209d8916f8e88ab6abd3c4218993344
Yoonyesol <maniac970430@naver.com>
2023-04-10 12:11:54 +0900

    [BOJ] 6186_Best Grass 코드 추가

--- [백준] 6186_Best Grass.py
+++ [백준] 6186_Best Grass.py
@@ -31,3 +31,40 @@ for i in range(r):
             bfs(i, j)
             answer += 1
 print(answer)
+
+
+-----------------
+
+import sys
+from collections import deque
+r, c = map(int, sys.stdin.readline().split())
+grass = []
+for _ in range(r):
+    grass.append(list(sys.stdin.readline().rstrip()))
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    grass[x][y] = "."
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >= r or ny >= c or nx < 0 or ny < 0:
+                continue
+            if grass[nx][ny] == ".":
+                continue
+            if grass[nx][ny] == "#":
+                grass[nx][ny] = "."
+                queue.append((nx, ny))
+answer = 0
+for i in range(r):
+    for j in range(c):
+        if grass[i][j] == "#":
+            bfs(i, j)
+            answer += 1
+print(answer)
