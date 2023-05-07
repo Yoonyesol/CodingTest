857741209a5f776ee61913cf412374ce9a2c323c
Yoonyesol <maniac970430@naver.com>
2023-05-08 02:35:40 +0900

    [SWEA] 2805. 농작물 수확하기 / D3 / 57분 / 2S

--- [swea] 2805. 농작물 수확하기.py
+++ [swea] 2805. 농작물 수확하기.py
@@ -16,3 +16,27 @@ for test_case in range(1, T + 1):
             start+=1
             end-=1
     print("#{} {}".format(test_case, sum))
+
+
+--------------------------------
+
+23/05/08
+
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())    #농장의 크기
+    values = []
+    ans = 0
+    n_sum = 0
+    for i in range(n):
+        col = list(map(int, input().rstrip()))
+        n_sum += sum(col)
+        values.append(col)
+    for i in range(n):
+        if i < (n // 2):
+            n_sum -= sum(values[i][:n//2-i])
+            n_sum -= sum(values[i][-(n//2-i):])
+        elif i > (n // 2):
+            n_sum -= sum(values[i][:-(n//2-i)])
+            n_sum -= sum(values[i][(n//2-i):])
+    print("#{} {}".format(test_case, n_sum))
