a03343b70df1fdd22ec12393044a5eea0954d060
Yoonyesol <maniac970430@naver.com>
2023-05-08 23:12:27 +0900

    [SWEA] 1215. [S/W 문제해결 기본] 3일차 - 회문1 / D3 / 47분 / 2S

--- [swea] 1215. [SW 문제해결 기본] 3일차 - 회문1.py
+++ [swea] 1215. [SW 문제해결 기본] 3일차 - 회문1.py
@@ -20,3 +20,45 @@ for test_case in range(1, T+1):
             if char == char[::-1]:
                 cnt += 1
     print("#{} {}".format(test_case, cnt))
+
+
+----------------------------------
+
+23.05.08
+
+import math
+
+for test_case in range(1, 11):
+    answer = 0
+    hoi = int(input())
+    pan = []
+
+    for i in range(8):
+        pan.append(list(input().rstrip()))
+    pan2 = list(zip(*pan))
+
+    for i in range(8):
+        for j in range(9-hoi):
+            p_list = pan[i][j:hoi + j]
+            p_list2 = pan2[i][j:hoi + j]
+            start = 0
+            end = hoi - 1
+            for _ in range(int(math.ceil(hoi/2))):
+                if p_list[start] != p_list[end]:
+                    break
+                start += 1
+                end -= 1
+            else:
+                answer += 1
+
+            start2 = 0
+            end2 = hoi - 1
+            for _ in range(int(math.ceil(hoi/2))):
+                if p_list2[start2] != p_list2[end2]:
+                    break
+                start2 += 1
+                end2 -= 1
+            else:
+                answer += 1
+
+    print("#{} {}".format(test_case, answer))
