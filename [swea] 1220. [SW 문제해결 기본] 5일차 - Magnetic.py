7b05cfc59092609d532bd4a06ac3569cb6eaeae6
Yoonyesol <maniac970430@naver.com>
2023-05-10 00:33:23 +0900

    [SWEA] 1220. [S/W 문제해결 기본] 5일차 - Magnetic / D3 / 41분 / 2S

--- [swea] 1220. [SW 문제해결 기본] 5일차 - Magnetic.py
+++ [swea] 1220. [SW 문제해결 기본] 5일차 - Magnetic.py
@@ -12,3 +12,39 @@ for test_case in range(1, T + 1):
             elif stack and mgt[r][c] == 2:	#스택에 1이 들어있고 S극(파란색, 2)인 경우
                 cnt += stack.pop()	#교착상태 짝이 맞으므로 1 pop
     print("#{} {}".format(test_case, cnt))
+
+
+----------------------------------------
+
+
+T = 10
+for test_case in range(1, T + 1):
+    _ = int(input())
+    arr = []
+    ans = 0
+    
+    for _ in range(100):
+        arr.append(list(map(int, input().split())))
+        
+    for i in range(100):
+        n_flag = 0
+        s_flag = 0
+        for j in range(100):
+            if arr[j][i] == 1:
+                n_flag = 1
+                s_flag = 0
+            elif arr[j][i] == 2:
+                s_flag = 1
+                n_flag = 0
+            if arr[j][i] == 0:
+                if n_flag == 1:
+                    arr[j][i] = 1
+                elif s_flag == 1:
+                    arr[j][i] = 2
+                    
+    for i in range(100):
+        for j in range(99):
+            if arr[j][i] == 1 and arr[j+1][i] == 2:
+                ans += 1
+                
+    print("#{} {}".format(test_case, ans))
