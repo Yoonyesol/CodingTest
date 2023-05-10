9203f9e735c39ab8dbd62b422f543cc6e9d3d71e
Yoonyesol <maniac970430@naver.com>
2023-05-10 23:33:19 +0900

    [SWEA] 1225. [S/W 문제해결 기본] 7일차 - 암호생성기 / D3 / 31분 / 2S

--- [swea] 1225. [SW 문제해결 기본] 7일차 - 암호생성기.py
+++ [swea] 1225. [SW 문제해결 기본] 7일차 - 암호생성기.py
@@ -12,3 +12,24 @@ for test_case in range(1, T + 1):
             break
         i += 1
     print("#{} {} {} {} {} {} {} {} {}".format(tc, *data))
+
+
+-----------------------------------
+
+
+T = 10
+for test_case in range(1, T + 1):
+    _ = int(input())
+    arr = [i for i in list(map(int, input().split()))]
+    i = 1
+    while True:
+        a = arr.pop(0)-i
+        if a <= 0:
+            arr.append(0)
+            break
+        else:
+            arr.append(a)
+        i += 1
+        if i > 5:
+            i = 1
+    print("#{} {}".format(test_case, ' '.join(map(str, arr))))
