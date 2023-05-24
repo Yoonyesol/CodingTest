ab801a47492a51dbd08ea676719de735c24d4b9a
Yoonyesol <maniac970430@naver.com>
2023-05-24 10:16:14 +0900

    [PGM] 모음사전 / level 2 / 41분

---
+++ [프로그래머스] 완전탐색_모음사전.py
@@ -0,0 +1,8 @@
+from itertools import product
+def solution(word):
+    answer = []
+    for i in range(1, 6):
+        for j in product(["A", "E", "I", "O", "U"], repeat = i):
+            answer.append("".join(j))
+    answer.sort()
+    return answer.index(word)+1
