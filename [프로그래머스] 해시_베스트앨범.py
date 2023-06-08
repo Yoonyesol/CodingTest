40a1edb747df8eb67969efe7474b6adcf2041e38
Yoonyesol <maniac970430@naver.com>
2023-06-08 17:22:19 +0900

    [PGM] 베스트앨범 / level 3 / 30분 / 2S

--- [프로그래머스] 해시_베스트앨범.py
+++ [프로그래머스] 해시_베스트앨범.py
@@ -8,3 +8,24 @@ def solution(genres, plays):
         temp = [i[0] for i in sorted(dic[g], key=lambda x:(x[1], -x[0]), reverse=True)]
         answer += temp[:min(2, len(temp))]
     return answer
+
+----------------------
+
+def solution(genres, plays):
+    answer = []
+    dic1 = {}
+    dic2 = {genre:0 for genre in set(genres)}
+    for idx, (i, j) in enumerate(zip(genres, plays)):
+        dic1[idx] = (i, j)
+        dic2[i] += j
+    dic1 = sorted(dic1.items(), key = lambda x:-x[1][1])
+    dic2 = sorted(dic2.items(), key = lambda x:-x[1])
+    for i in dic2:
+        cnt = 0
+        for j in dic1:
+            if j[1][0] == i[0]:
+                answer.append(j[0])
+                cnt += 1
+            if cnt == 2:
+                break
+    return answer
