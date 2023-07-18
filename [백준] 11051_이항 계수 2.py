7d314e6b1896e0eb778605eb8703705dd38df7be
Yoonyesol <maniac970430@naver.com>
2023-07-18 22:32:32 +0900

    [BOJ] 11051_이항 계수 2 / 실버2 / 12분 / 2

--- [백준] 11051_이항 계수 2.py
+++ [백준] 11051_이항 계수 2.py
@@ -9,3 +9,19 @@ for i in range(n+1):
         else:   #이항계수 점화식 그대로 코드화
             dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%10007
 print(dp[n][k]%10007)
+
+------------------------
+
+import sys
+input=sys.stdin.readline
+n, k = map(int, input().split())
+dp = [[0]*(k+1) for _ in range(n+1)]
+for i in range(n+1):
+    for j in range(k+1):
+        if j==0 or i==j:
+            dp[i][j] = 1
+        else:
+            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%10007
+print(dp[n][k]%10007)
+
+
