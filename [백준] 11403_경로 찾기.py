681536f252ef47506a51840e49b54e22aee4df6f
Yoonyesol <maniac970430@naver.com>
2023-07-25 23:00:56 +0900

    [BOJ] 11403_경로 찾기 / 실버1 / 1시간 2분 / 2

--- [백준] 11403_경로 찾기.py
+++ [백준] 11403_경로 찾기.py
@@ -19,3 +19,32 @@ for i in range(N):
         print(visited[j], end=' ')
     print()
     visited = [0 for _ in range(N)]
+
+--------------------
+
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+def bfs(start):
+    queue = deque()
+    queue.append(start) #시작 정점을 queue에 삽입
+    visited = [0]*n #방문한 노드를 저장할 리스트 생성 및 초기화
+    while queue:    #queue가 빌 때까지(가능한 모든 이동가능한 경로를 찾을 때까지)
+        node = queue.popleft()  #먼저 들어온 값부터 꺼냄
+        for i in graph[node]:   #현재 노드에 연결된 모든 노드를 확인
+            if not visited[i]:  #방문하지 않은 노드가 있다면
+                visited[i] = 1  #방문처리
+                queue.append(i) #현재 노드에서부터 다른 노드로의 경로도 확인하기 위해 현재 노드를 queue에 저장
+    print(*visited) #방문처리된 결과를 출력(시작노드에서 방문가능한 경로라면 1로 표시되고, 아닌 경우 값이 초기화 값인 0으로 유지된다)
+
+n = int(input())
+graph = [[] for _ in range(n)]  #bfs로 순회할 그래프 생성 및 초기화
+for i in range(n):
+    arr = list(map(int, input().split()))   #행렬을 한줄씩 리스트 형태로 입력받기
+    for j in range(n):  #입력받은 리스트의 원소를 확인
+        if arr[j] == 1: #리스트의 원소가 1이라면,
+            graph[i].append(j)  #i에서 j로 이동하는 것이 가능하다는 뜻이므로 그래프에 이동가능 경로 표시(유향그래프이므로, graph[j].append(i)는 진행하지 않는다.)
+
+for i in range(n):  #그래프의 모든 정점에 대해 bfs 실행
+    bfs(i)  #시작노드는 현재 정점
