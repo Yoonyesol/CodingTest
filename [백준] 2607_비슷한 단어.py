1b50265abe3765cbdeb6edda950d17847c818457
Yoonyesol <maniac970430@naver.com>
2023-08-04 15:02:36 +0900

    [BOJ] 2607_비슷한 단어 / 실버3 / 6분 / 2

--- [백준] 2607_비슷한 단어.py
+++ [백준] 2607_비슷한 단어.py
@@ -16,3 +16,23 @@ for _ in range(n-1):
     if cnt < 2 and len(compare) < 2:
         ans += 1
 print(ans)
+
+----------------------------
+
+import sys
+input=sys.stdin.readline
+n = int(input())
+first = list(input().rstrip())
+ans = 0
+for _ in range(n-1):
+    dup = first.copy()
+    word = input().rstrip()
+    cnt = 0
+    for i in word:
+        if i in dup:
+            dup.remove(i)
+        else:
+            cnt += 1
+    if cnt < 2 and len(dup) < 2:
+        ans += 1
+print(ans)
