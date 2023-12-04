6833d4309c5498e0c556c8d38c5bc167cab05149
Yoonyesol <maniac970430@naver.com>
2023-12-04 22:34:47 +0900

    [BOJ] 19941_햄버거 분배 / 실버3 / 37분 / 2

--- [백준] 19941_햄버거 분배.py
+++ [백준] 19941_햄버거 분배.py
@@ -13,3 +13,24 @@ for i in range(n):  #모든 리스트를 순회
                 ans += 1    #먹은 만큼 ans값 증가
                 break   #다른 햄버거를 먹지 않게 반복문 종료, 다른 P에게 H를 줄 수 있을지 찾는다.
 print(ans)
+
+
+-------------------
+<2차>
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+bench = list(input().rstrip())
+
+ans = 0
+for i in range(n):
+    if bench[i] == 'P':
+        # 어차피 왼쪽 구간에서 h 발견하면 break되니까 끊지 말고 +-k구간 전부 반복해서 탐색
+        for j in range(max(0, i-k), min(i + k + 1, n)):
+            if bench[j] == 'H':
+                bench[j] = 0    #먹은 햄버거는 먹음 처리
+                ans += 1
+                break
+print(ans)
