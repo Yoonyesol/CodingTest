bcaa423f36d6a7078ea172ebe48a7eff28454cda
Yoonyesol <51500821+Yoonyesol@users.noreply.github.com>
2025-06-12 02:41:04 +0900

    Title: 반복 출력하기, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250612/반복 출력하기/repeated-output.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+for(let i=0; i<n; i++){
+    console.log("12345^&*()_")
+}
